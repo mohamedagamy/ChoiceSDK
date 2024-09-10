0c71e236bc262588f216aa9951e1267a690c644b
magamy <mohamed.elagamy@segmatek.com>
2024-09-11 01:11:41 +0300

    fix mapkit init bug.

--- ChoiceSDK/choicesdk-app/src/main/java/com/bluesource/choicesdk_app1/ChoiceSdkApp.kt
+++ ChoiceSDK/choicesdk-app/src/main/java/com/bluesource/choicesdk_app1/ChoiceSdkApp.kt
@@ -3,11 +3,13 @@ package com.bluesource.choicesdk_app1
 import android.app.Application
 import at.bluesource.choicesdk.core.ChoiceSdk
 import at.bluesource.choicesdk.core.MobileService
+import at.bluesource.choicesdk.maps.common.MapsInitializer
 
 class ChoiceSdkApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
         ChoiceSdk.init(this, listOf(MobileService.HMS))
+        MapsInitializer.initialize(this)
     }
 }
--- ChoiceSDK/choicesdk-maps/src/main/java/at/bluesource/choicesdk/maps/common/MapsInitializer.kt
+++ ChoiceSDK/choicesdk-maps/src/main/java/at/bluesource/choicesdk/maps/common/MapsInitializer.kt
@@ -11,7 +11,11 @@ class MapsInitializer {
         fun initialize(context: Context) {
             when (MobileServicesDetector.getAvailableMobileService()) {
                 MobileService.GMS -> com.google.android.gms.maps.MapsInitializer.initialize(context)
-                MobileService.HMS -> com.huawei.hms.maps.MapsInitializer.initialize(context)
+                MobileService.HMS -> {
+                    com.huawei.hms.maps.MapsInitializer.initialize(context)
+                    com.huawei.hms.maps.MapsInitializer.setApiKey("DQEDAO1RDIyn/7m+s/xUka/sJ822LulwH7xsXXfGIIykR66u/RsAf35HX003nhljH6qlSxoq9FG/G37vpp4M4Hu0AihfW3rq9Wwy1A==")
+                    com.huawei.hms.maps.MapsInitializer.setAppId("111881535")
+                }
             }
         }
     }
